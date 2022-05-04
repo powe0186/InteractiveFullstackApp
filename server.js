0fc1c3535eb1d66121f089aa61c83abe0875a695
Ben Powell <powe0186@gmail.com>
2022-05-04 09:35:01 -0500

    started server.js file - need to research newest v. of connect-express-store

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const Sequelize = require("sequelize");  // Added this from documentation for connect-session-sequelize
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+//set up session:
