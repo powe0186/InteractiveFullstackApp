e5178268284c3f0dab3952a56f9a60cbd1c4f6dc
Ben Powell <powe0186@gmail.com>
2022-05-05 19:38:12 -0500

    C:/Program Files/Git/post POST route working

--- controllers/api/postsRoutes.js
+++ controllers/api/postsRoutes.js
@@ -4,13 +4,29 @@ const { Post } = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
-        console.log('ROUTE ENGAGED');
         const posts = await Post.findAll();
         res.status(200).json(posts);
     } catch (err) {
         console.error(err)
         res.status(400).json(err);
     }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create(
+            {
+                title: req.body.title,
+                message: req.body.message,
+                user_id: req.body.user_id //Change this!
+            }
+        );
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 })
+
+
 
 module.exports = router;
